53ee1f33aad3a80c7d51b7db0ebe0cdf99f6f1e5
Clark Joy Pesco <clarkjoypesco@gmail.com>
2025-01-04 14:46:55 +0800

    Added Env file for database

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -7,6 +7,10 @@ require __DIR__ . "/../../vendor/autoload.php";
 use Framework\App;
 use App\Config\Paths;
 use function App\Config\{registerRoutes, registerMiddleware};
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
 
 $app = new App(Paths::SOURCE . "App/container-definitions.php");
 
